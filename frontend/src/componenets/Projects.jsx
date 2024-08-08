5ba3a15a21499a4ca674adeacff65cf23fb0f1b2
Manikandan <username270904@gmail.com>
2024-08-08 17:05:05 +0530

    second commit of day merging 2 branches

--- frontend/src/componenets/Projects.jsx
+++ frontend/src/componenets/Projects.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import './projects.css'
+import new_collections from '../assets/P_assets/new_collections.js'
 function Projects() {
   return (
-    <div>
+    <div className="product-grid">
+      {new_collections.map((product) => (
+        <div className="product-card" key={product.id}>
+          <img src={product.image} alt={product.name} className="product-image" />
+          <h2 className="product-name">{product.name}</h2>
+          <p className="product-price">
+            <span className="new-price">${product.new_price.toFixed(2)}</span>
+            <span className="old-price">${product.old_price.toFixed(2)}</span>
+          </p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
+
 
 export default Projects
